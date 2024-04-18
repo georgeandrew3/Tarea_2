6bbb90931eab40071e78ac2ebb08de39c0aab864
georgeandrew3 <jorgfigueroa@alumnos.uai.cl>
2024-04-18 19:39:07 -0400

    Update Codigo.c

--- Codigo.c
+++ Codigo.c
@@ -5,6 +5,17 @@
 #define MOTOR2_IN1 7
 #define MOTOR2_IN2 8
 
+
+#define BOTON_ADELANTE 9
+#define BOTON_ATRAS 10
+#define BOTON_IZQUIERDA 11
+#define BOTON_DERECHA 12
+
+int motorSpeed= 150; 
+  
+void setup() {
+  
+  pinMode(MOTOR1_ENABLE, OUTPUT);
   pinMode(MOTOR1_IN1, OUTPUT);
   pinMode(MOTOR1_IN2, OUTPUT);
   pinMode(MOTOR2_ENABLE, OUTPUT);
@@ -24,7 +35,9 @@ void loop() {
   int atrasState = digitalRead(BOTON_ATRAS);
   int izquierdaState = digitalRead(BOTON_IZQUIERDA);
   int derechaState = digitalRead(BOTON_DERECHA);
-
+  
+  analogWrite(MOTOR1_ENABLE, motorSpeed);
+  analogWrite(MOTOR2_ENABLE, motorSpeed);
   // adelante
   if (adelanteState == HIGH) {
     digitalWrite(MOTOR1_IN1, LOW);
